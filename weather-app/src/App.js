b1eefde2a9a30e8f3f56f58683d1c03231549745
MiguelBalderrama97 <37118731+MiguelBalderrama97@users.noreply.github.com>
2019-07-03 11:24:37 -0600

    Multiples componentes e importaciones

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -6,7 +6,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Que pex!
       <WeatherLocation></WeatherLocation>
     </div>
   );
--- weather-app/src/components/weather-location.js
+++ weather-app/src/components/weather-location.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import Location from "./location";
+import WeatherData from "./weather-data";
 
 const WeatherLocation = () => (
-    <div>Weather Location</div>
+    <div>
+        <Location></Location>
+        <WeatherData></WeatherData>
+    </div>
 );
+
+// ESTO ES LO MISMO QUE LA ARROW FUNCTION DE ARRIBA!
+// function WeatherLocation(){
+//     return <div>Weather Location</div>
+// }
 
 export default WeatherLocation;
