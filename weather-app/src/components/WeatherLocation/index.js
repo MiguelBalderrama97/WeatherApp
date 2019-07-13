f06949d7ab836e670639a3642122571ba526c04b
MiguelBalderrama97 <37118731+MiguelBalderrama97@users.noreply.github.com>
2019-07-13 15:01:42 -0600

    OnClick y State

--- weather-app/src/components/WeatherLocation/index.js
+++ weather-app/src/components/WeatherLocation/index.js
@@ -18,6 +18,13 @@ const data = {
     wind: '10 m/s',
 }
 
+const data2 = {
+    temperature: 18,
+    weatherState: WINDY,
+    humidity: 20,
+    wind: '15 m/s',
+}
+
 /*const WeatherLocation = ({city}) => (
     <div className="WeatherLocationCont">
         <Location city={city}></Location>
@@ -27,11 +34,32 @@ const data = {
 
 //ESTE ES UN CLASS COMPONENT, EL DE ARRIBA ES UN FUNCTIONAL COMPONENT
 class WeatherLocation extends Component{
+
+    constructor(){
+        super();
+        this.state = {
+            city: "Chihuahua",
+            data: data,
+        }
+    }
+
+    handleUpdateClick = () => {
+        console.log("Actualizado...");
+        this.setState({
+            city: "Coahuila",
+            data: data2,
+        });
+    }
+
     render(){
+
+        const {city, data} = this.state;
+
         return(
             <div className="WeatherLocationCont">
-                <Location></Location>
+                <Location city={city}></Location>
                 <WeatherData data={data}></WeatherData>
+                <button onClick={this.handleUpdateClick}>Actualizar</button>
             </div>
         );
     }
